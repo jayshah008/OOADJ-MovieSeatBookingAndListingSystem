d357d9e8cb094ad207e22b2840c3f815747828c7
jayshah008 <104262682+jayshah008@users.noreply.github.com>
2024-04-13 21:01:06 +0530

    Update MovieApp.java

--- MovieApp.java
+++ MovieApp.java
@@ -4,24 +4,13 @@ import src.handler.*;
 import src.view.*;
 
 
-/**
- * The start of the MOBLIMA Application
- * @author Lee Juin
- * @version 1.0
- */
+
 public class MovieApp {
 
-    /**
-     * Default constructor
-     */
     public MovieApp(){}
     
     private static MovieAppView defaultView = new MovieAppView();
 
-    /**
-     * Main driver function for the application
-     * @param args is the arguments to be passed into the function
-     */
     public static void main(String args[]) {
         UIHandler.clearScreen();
         System.out.println("");
