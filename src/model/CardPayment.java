8ad4cd3234deca2d6e09a4e0d59bb235634d7b1b
Jawahar Balachandher <jawahar.balachandher@gmail.com>
2024-04-14 15:55:08 +0530

    Add CardPayment.java

---
+++ src/model/CardPayment.java
@@ -0,0 +1,31 @@
+package src.model;
+
+import src.model.enums.PaymentType;
+
+public class CardPayment extends Payment {
+    private PaymentType paymentType;
+
+    private double movieTicketPrice;
+
+    public CardPayment(String UUID, PaymentType paymentType, String transactionID, double movieTicketPrice) {
+        super(UUID, transactionID);
+        this.setMovieTicketPrice(movieTicketPrice);
+        this.setPaymentType(paymentType);
+    }
+
+    public PaymentType getPaymentType() {
+        return this.paymentType;
+    }
+
+    public void setPaymentType(PaymentType paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public double getMovieTicketPrice() {
+        return this.movieTicketPrice;
+    }
+
+    public void setMovieTicketPrice(double movieTicketPrice) {
+        this.movieTicketPrice = movieTicketPrice;
+    }
+}
